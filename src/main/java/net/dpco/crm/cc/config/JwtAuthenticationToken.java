f6cab81297c7ba1c5f26b3aa48eb50950613c9ae
omid rahmani <o.rahmani@fwutech.com>
2020-09-29 07:59:27 +0330

    add jwt xml in project

---
+++ src/main/java/net/dpco/crm/cc/config/JwtAuthenticationToken.java
@@ -0,0 +1,33 @@
+package net.dpco.crm.cc.config;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+import java.util.Collection;
+
+public class JwtAuthenticationToken extends UsernamePasswordAuthenticationToken {
+
+
+    private String token;
+
+    public JwtAuthenticationToken(String token) {
+        super(null, null);
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return null;
+    }
+}
